7f7f9a9fd63c111c979ac8f37c8ba89ec59e8ade
beefchimi <curtdulmage@gmail.com>
2016-02-06 23:48:27 -0500

    added new search styles

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -36,6 +36,68 @@ document.addEventListener('DOMContentLoaded', function() {
     boolRetryRequest = false;
 
 
+  // initSearchTypeahead: Temporary typeahead.js test function
+  // ----------------------------------------------------------------------------
+  function initSearchTypeahead() {
+
+    var substringMatcher = function(strs) {
+      return function findMatches(q, cb) {
+        var matches, substringRegex;
+
+        // an array that will be populated with substring matches
+        matches = [];
+
+        // regex used to determine if a string contains the substring `q`
+        substrRegex = new RegExp(q, 'i');
+
+        // iterate through the pool of strings and for any string that
+        // contains the substring `q`, add it to the `matches` array
+        $.each(strs, function(i, str) {
+          if (substrRegex.test(str)) {
+            matches.push(str);
+          }
+        });
+
+        cb(matches);
+      };
+    };
+
+    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+      'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+      'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+      'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+      'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+      'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+      'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+      'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+      'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+    ];
+
+    $('#filter_search').typeahead({
+      hint: true,
+      minLength: 1
+    },
+    {
+      name: 'states',
+      source: substringMatcher(states)
+    });
+
+  }
+
+
+  // searchFocus: Required now that typeahead.js wraps our input in a <span>
+  // ----------------------------------------------------------------------------
+  function searchFocus() {
+    var elForm = document.getElementById('search_projects');
+    elForm.addEventListener('focus', function(event) {
+      elForm.classList.add('is-focused');
+    }, true);
+    elForm.addEventListener('blur', function( event ) {
+      elForm.classList.remove('is-focused');
+    }, true);
+  }
+
+
   // onPageLoad: Main Function To Fire on Window Load
   // ----------------------------------------------------------------------------
   function onPageLoad() {
@@ -54,6 +116,11 @@ document.addEventListener('DOMContentLoaded', function() {
     cycleLinkColors();
     paginationEllipsis(7); // will not run if window width is above 768px
     error404Video();
+
+    if (elBody.classList.contains('home')) {
+      initSearchTypeahead();
+      searchFocus();
+    }
 
   }
 
